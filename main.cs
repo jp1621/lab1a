d937b85a37b56cb9d3bf27f1f405f52bfd0a7807
jp1621 <jimmy_pham1998@yahoo.com>
2020-06-17 02:58:15 +0000

    Initial commit

---
+++ main.cs
@@ -0,0 +1,13 @@
+using System;
+
+class MainClass {
+  public static void Main (string[] args) {
+    Console.WriteLine ("Enter the name of item");
+    Console.WriteLine ("apple");
+    Console.WriteLine ("Enter the quality");
+    Console.WriteLine ("15");
+    Console.WriteLine ("Enter the price");
+    Console.WriteLine ("0.75");
+    Console.WriteLine ("Total price of apple= $15");
+  }
+}
